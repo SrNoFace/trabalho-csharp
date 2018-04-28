08ff7e663d9a679e2e6fdd4a1e35d3d2e36cf469
Matheus Rambo <33197461+rambosaiyan3@users.noreply.github.com>
2018-04-28 17:19:40 -0300

    Alterações

--- ListarPessoa.aspx.cs
+++ ListarPessoa.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using RamboErp.dao;
+using System.Web.UI.HtmlControls;
 
 namespace RamboErp
 {
@@ -15,11 +16,27 @@ namespace RamboErp
 
         }
 
-        protected void listar_Click(object sender, EventArgs e)
+        private void geraTabela()
         {
+            HtmlTable tabela = new HtmlTable();
+            HtmlTableRow linha;
+            HtmlTableCell coluna;
             PessoaDao pessoaDao = new PessoaDao();
-            string dados = pessoaDao.buscaDados();
-            Response.Write(dados);
+            int numero = pessoaDao.buscarPessoas().Length;
+
+            for (int i = 0; i < numero; i++)
+            {
+                linha = new HtmlTableRow();
+                for (int z = 0; z < 9; z++)
+                {
+                    coluna = new HtmlTableCell();
+                    coluna.InnerHtml = pessoaDao.buscarPessoas()[z].ToString();
+                    linha.Cells.Add(coluna);
+                }
+                tabela.Rows.Add(linha);
+
+            }
+            Controls.Add(tabela);
         }
     }
 }
